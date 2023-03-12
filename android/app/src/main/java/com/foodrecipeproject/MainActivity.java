0623e39d3a27d23b84e41a0567e828671296ba7d
NutanDobariya <npdobariya88@gmail.com>
2023-03-12 18:26:20 +0530

    added splash screen for both platform

--- android/app/src/main/java/com/foodrecipeproject/MainActivity.java
+++ android/app/src/main/java/com/foodrecipeproject/MainActivity.java
@@ -1,11 +1,18 @@
 package com.foodrecipeproject;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
